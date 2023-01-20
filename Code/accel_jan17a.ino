b23347f3b0780602ba277c8274a43064cdc3118f
David Wamai <davwamai@gmail.com>
2023-01-20 15:13:42 -0500

    finalization

--- Code/accel_jan17a.ino
+++ Code/accel_jan17a.ino
@@ -6,6 +6,7 @@
 Adafruit_MPU6050 mpu;
 int CS = 10;
 File file; 
+unsigned long time;
 
 void setup(void) {
   Serial.begin(115200);
@@ -35,10 +36,14 @@ void setup(void) {
 
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
+
+
   delay(100);
 }
 
 void loop() {
+
+  
 
   // Get new sensor events with the readings
   // expects three params for some reason  
@@ -48,11 +53,16 @@ void loop() {
   //prints to file
   file =  SD.open("data.txt", FILE_WRITE);
   if(file){
+  //   if(millis()/1000!= time){
+  //   time = millis()/1000;
+  //   Serial.print(uptime(millis()));
+  // }
+    file.print("\t");
     file.print(a.acceleration.x); 
     file.print("\t");
     file.print(a.acceleration.y); 
     file.print("\t");
-    file.print(a.acceleration.z);
+    file.println(a.acceleration.z);
     file.close();
   } else {
     Serial.println("Could not open file...");
@@ -65,4 +75,16 @@ void loop() {
   Serial.print("\t");
   Serial.println(a.acceleration.z);
   delay(100);
+}
+
+char *uptime(unsigned long milli){
+  static char _return[32];
+  unsigned long secs=milli/1000, mins=secs/60;
+  unsigned int hours=mins/60, days=hours/24;
+  milli-=secs*1000;
+  secs-=mins*60;
+  mins-=hours*60;
+  hours-=days*24;
+  sprintf(_return, "Uptime: %d days %2.2d:%2.2d:%2.2d:%3.3d", (byte)days, (byte)hours, (byte)mins, (byte)secs, (int)milli);
+  return _return; 
 }
